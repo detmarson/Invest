f19599add5e0f01af8ab7baf31cadf2e949654fe
Marcin Detlaf <marcin.detlaf7@gmail.com>
2023-09-27 11:00:19 +0200

    Created Syntax

---
+++ js/main.js
@@ -0,0 +1,16 @@
+const navMoblie = document.querySelector('.nav-mobile');
+const navBtn = document.querySelector('.hamburger');
+const footerYear = document.querySelector('.footer__year');
+
+const handleNav = () => {
+	navBtn.classList.toggle('is-active');
+	navMoblie.classList.toggle('nav-mobile--active');
+};
+
+navBtn.addEventListener('click', handleNav);
+
+const handleCurrentYear = () => {
+	const year = new Date().getFullYear();
+	footerYear.innerText = year;
+};
+handleCurrentYear();
